e28e15beb53a7de83622445394ac301a4f97d695
DlopezTransactRx <dlopez@transactrx.com>
2025-09-24 16:16:14 -0400

    Lexis Nexsis Update

--- Postgress/LexsisNexsis.sql
+++ Postgress/LexsisNexsis.sql
@@ -11,6 +11,11 @@ CREATE TABLE test_hms_address AS
 SELECT * FROM hms_address;
 
 ----------------------------------------------------------------------------------------------------
+-- Drop TestJob Stage
+----------------------------------------------------------------------------------------------------
+DROP TABLE ftp_stage_testjob;
+
+----------------------------------------------------------------------------------------------------
 -- Counts
 ----------------------------------------------------------------------------------------------------
 SELECT COUNT(*) FROM hms_address;
@@ -24,9 +29,8 @@ SELECT COUNT(*) FROM hms_practitioner_profile;
 
 
 ----------------------------------------------------------------------------------------------------
--- Scratch
+-- hms_practitioner_profile
 ----------------------------------------------------------------------------------------------------
-
 SELECT
   SPLIT_PART(data, '|', 1)  AS HMS_PIID,
   SPLIT_PART(data, '|', 2)  AS FIRST,
@@ -41,6 +45,38 @@ SELECT
 FROM ftp_stage_testjob
 ;
 
-DROP TABLE ftp_stage_testjob;
 
--- HMS_PIID|FIRST|MIDDLE|LAST|SUFFIX|CRED|PRACTITIONER_TYPE|UPIN|NPI|CHNG_FLAG
+----------------------------------------------------------------------------------------------------
+-- hms_dea
+----------------------------------------------------------------------------------------------------
+SELECT
+    SPLIT_PART(data, '|', 1)  AS HMS_PIID,
+    SPLIT_PART(data, '|', 2)  AS DEA,
+    SPLIT_PART(data, '|', 3)  AS DEA_SCHEDULE,
+    SPLIT_PART(data, '|', 4)  AS DEA_EXPIRE,
+    SPLIT_PART(data, '|', 5)  AS NADEAN,
+    SPLIT_PART(data, '|', 6)  AS LID,
+    SPLIT_PART(data, '|', 7)  AS CHNG_FLAG
+FROM ftp_stage_testjob
+;
+
+----------------------------------------------------------------------------------------------------
+-- hms_address
+----------------------------------------------------------------------------------------------------
+SELECT
+    SPLIT_PART(data, '|', 1)  AS HMS_PIID,
+    SPLIT_PART(data, '|', 2)  AS HMS_LID,
+    SPLIT_PART(data, '|', 3)  AS RANK,
+    SPLIT_PART(data, '|', 4)  AS FIRM_NAME,
+    SPLIT_PART(data, '|', 5)  AS ADDRESS1,
+    SPLIT_PART(data, '|', 6)  AS ADDRESS2,
+    SPLIT_PART(data, '|', 7)  AS CITY,
+    SPLIT_PART(data, '|', 8)  AS STATE,
+    SPLIT_PART(data, '|', 9)  AS ZIP,
+    SPLIT_PART(data, '|', 10) AS ZIP4,
+    SPLIT_PART(data, '|', 11) AS PHONE1,
+    SPLIT_PART(data, '|', 12) AS PHONE2,
+    SPLIT_PART(data, '|', 13) AS FAX,
+    SPLIT_PART(data, '|', 14) AS CHNG_FLAG
+FROM ftp_stage_testjob
+;
