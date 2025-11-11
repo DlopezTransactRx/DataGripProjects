db643c52447052e2880921ab0d444c57d57fb403
DlopezTransactRx <dlopez@transactrx.com>
2025-11-11 11:15:04 -0500

    Updates

--- Batch Support/Pharmacy Switch/POSTGRES - ReSync - Pharmacy Switch.sql
+++ Batch Support/Pharmacy Switch/POSTGRES - ReSync - Pharmacy Switch.sql
@@ -1,4 +1,11 @@
---====================================================================================================
+-- ====================================================================================================
+-- [POSTGRES] NOTE: New Columns Added to Support PPE Rules Not In Snowflake.
+-- ====================================================================================================
+SELECT ppe_rule_base_id FROM pharmacy_switch_service where ppe_rule_base_id IS NOT NULL;
+SELECT direct_override, insert_intermediary_auth_code, ppe_type, record_status FROM switch_service;
+
+
+-- ====================================================================================================
 -- [POSTGRES] Source Table Counts
 -- ====================================================================================================
 
@@ -8,12 +15,26 @@ WHERE event_type IN (
     'PharmacySwitchServiceTableRecord',
     'SwitchServiceTableRecord'
 );
+-- Sample Data
+SELECT * FROM pharmacy_switch_service;
+SELECT * FROM switch_service;
+
+-- Describe Tables
+SELECT column_name, data_type, is_nullable, column_default, table_name
+FROM information_schema.columns
+WHERE table_name in
+(
+--     'pharmacy_switch_service'--,
+    'switch_service'
+)
+ORDER BY table_name, column_name
+;
 
 --====================================================================================================
 -- [POSTGRES] Modify Export Index
 -- ====================================================================================================
 
 -- Source Record Counts
-SELECT 'pharmacy_switch_service_cnt' as tableName, COUNT(*) as cnt FROM pharmacy_switch_service
+SELECT 'pharmacy_switch_service' as tableName, COUNT(*) as cnt FROM pharmacy_switch_service
 UNION ALL
 SELECT 'switch_service' as tableName, COUNT(*) as cnt FROM switch_service;
