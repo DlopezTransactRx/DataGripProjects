1eba10d3fe36d1a33ba3a337cc3a4ca31bd0a0b8
DlopezTransactRx <dlopez@transactrx.com>
2026-06-10 15:43:14 -0400

    Updates

--- Snowflake/TEMPLATE/SELECT - Task Meta.sql
+++ Snowflake/TEMPLATE/SELECT - Task Meta.sql
@@ -2,12 +2,13 @@
 -- Following is the select portion to parse the latest history from a task created record.
 -------------------------------------------------------------------------------
 SELECT
-    TASK_META[0]:ROOT_TASK_NAME    as ROOT_TASK_NAME,
-    TASK_META[0]:ROOT_TASK_UUID    as ROOT_TASK_UUID,
-    TASK_META[0]:RUN_GROUP_ID      as RUN_GROUP_ID,
-    TASK_META[0]:RUN_TIME          as RUN_TIME,
-    TASK_META[0]:SCHEDULED_TIME    as SCHEDULED_TIME,
-    TASK_META[0]:TASK_NAME         as TASK_NAME,
+    TASK_META[0]:ROOT_TASK_NAME    as _TASKMETA_ROOT_TASK_NAME,
+    TASK_META[0]:ROOT_TASK_UUID    as _TASKMETA_ROOT_TASK_UUID,
+    TASK_META[0]:RUN_GROUP_ID      as _TASKMETA_RUN_GROUP_ID,
+    TASK_META[0]:RUN_TIME          as _TASKMETA_RUN_TIME,
+    TASK_META[0]:SCHEDULED_TIME    as _TASKMETA_SCHEDULED_TIME,
+    TASK_META[0]:TASK_NAME         as _TASKMETA_TASK_NAME,
+--     * EXCLUDE (TASK_META)
     *
 FROM
     <TABLE>
